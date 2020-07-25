6a81effb8ae8e6ff4785449b3c4fbeb91216d9a8
javier <javiermontes1997@hotmail.com>
2020-07-26 00:47:28 +0200

    Titulo, estilizado , comienzo de animacion del ganador

--- js/index.js
+++ js/index.js
@@ -4,23 +4,41 @@ const scrollBox = document.querySelector('.scrollbox .items');
 const items = clnItemsDisponibles.querySelectorAll('.item');
 const itemsArray = Array.from(items);
 const numElementosDiponibles = contItemsDisponibles.childElementCount;
+const button = document.querySelector('button'); 
+let nDivsAnterioresAlGanador;
 
 for (let i=0; i <  numElementosDiponibles * 7 ; i++) {
     itemsArray.forEach((element) => {
         scrollBox.appendChild(element.cloneNode(true));
     });   
 }
+function animacionWinner(nDivsAnteriores, zIndex, opacity){
+    scrollBox.querySelector(`div:nth-child(${nDivsAnteriores + 1})`).style.zIndex = `${zIndex}`;
+    document.querySelector('.padding .shadow').style.opacity = `${opacity}`;
+}
 
 function calcularGanador() {
-    let ganador = Math.floor(Math.random() * (numElementosDiponibles + 1 - 1)) + 1;
-    return (scrollBox.childElementCount - (numElementosDiponibles * 2)) + ganador - 1 ;
+    let ganador = Math.floor(Math.random() * (numElementosDiponibles)) + 1;
+    console.log('ganador:', ganador);
+    nDivsAnterioresAlGanador = (scrollBox.childElementCount - (numElementosDiponibles * 2)) + ganador - 1 ;
+    return nDivsAnterioresAlGanador;
 }
 function aplicarLeft() {
-    scrollBox.style.left = '0px';
     let leftCalculado = 134 * calcularGanador() - (134 * 2); 
+    scrollBox.style.transition = 'left 5s ease';
     scrollBox.style.left = `-${leftCalculado}px`;
+
 }
-document.querySelector('button')
-    .addEventListener('click', (e) => {
-        aplicarLeft();
-    })
+button
+.addEventListener('click', () => {
+        button.disabled = true;
+        if (nDivsAnterioresAlGanador) animacionWinner(nDivsAnterioresAlGanador, 0, 0 );
+        scrollBox.style.transition = 'left 0s ease';
+        scrollBox.style.left = '0px';
+        setTimeout(() => {aplicarLeft()}, 10);
+    });
+// Habilitamos el botón de nuevo cuando la animación concluye, para no poder rollear mientras estás todavía en el proceso     
+scrollBox.addEventListener('transitionend' , () => {
+    button.disabled = false;
+    animacionWinner(nDivsAnterioresAlGanador, 10, .9);
+});
